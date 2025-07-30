1942ef719ca64ac6269d699ef9b11cc3ddd5d852
rjrahul007 <rjrahool007@gmail.com>
2025-07-30 17:42:16 +0530

    Add constants/i ndex.ts

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-100 font-quicksand-bold">
-        Welcome to Nativewind!
+      <Text className="text-xl font-bold text-red-700 font-quicksand-bold">
+        Welcome to Food-delivery App!
       </Text>
     </View>
   );
