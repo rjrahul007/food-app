7c4e9099574394347db7366481160e3a195db9b6
rjrahul007 <rjrahool007@gmail.com>
2025-07-31 11:36:21 +0530

    Add fonts, custom css and splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
 
+
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "quicksand-bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "quicksand-semi-bold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "quicksand-regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "quicksand-medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "quicksand-light": require("../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+  
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-700 font-quicksand-bold">
+      <Text className="text-5xl font-bold text-primary text-center font-quicksand-bold">
         Welcome to Food-delivery App!
       </Text>
     </View>
